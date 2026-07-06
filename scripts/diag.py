bd7d4f86b9775b66d089eacc8f18425354afd9a3
marcol-create <marcol@alumni.stanford.edu>
2026-07-06 16:53:13 -0700

    Update diag.py

--- scripts/diag.py
+++ scripts/diag.py
@@ -1,11 +1,2 @@
-try:
-    db
-except NameError:
-    import compolyx
-    db = compolyx.DB()
-model = db.active_model
-raise RuntimeError(
-    "EDGE SETS: [%s]  ||  SOLID MODELS: [%s]  ||  ROSETTES: [%s]"
-    % (", ".join(model.edge_sets.keys()),
-       ", ".join(model.solid_models.keys()),
-       ", ".join(model.rosettes.keys())))
+raise RuntimeError(str([a for a in dir(seat_sm)
+    if 'guide' in a.lower() or 'xtrus' in a.lower()]))
