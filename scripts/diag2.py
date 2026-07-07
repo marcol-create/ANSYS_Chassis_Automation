5b5045d4259a0de64d826360bd0ec5cea2081c1c
marcol-create <marcol@alumni.stanford.edu>
2026-07-06 18:52:13 -0700

    Delete scripts/diag2.py

--- scripts/diag2.py
+++
@@ -1,5 +0,0 @@
-for a in dir(seat_sm):
-    if 'guide' in a.lower():
-        print(repr(a))
-raise RuntimeError("methods printed above -> also here: %s"
-    % [a for a in dir(seat_sm) if 'guide' in a.lower()])
